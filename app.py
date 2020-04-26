f7fab5ceb41bd4ab65460af7f5c111b4093cfa77
Sinrok Kim <fresh502@gmail.com>
2020-04-26 15:34:17 +0900

    apply api 추가

--- app.py
+++ app.py
@@ -1,57 +1,41 @@
 from pymongo import MongoClient
-from bs4 import BeautifulSoup
-import requests
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 
-# client = MongoClient('mongodb://sampleID:samplePW@12.34.56.78', 27017)
-client = MongoClient('mongodb://fellong:fellon@54.180.114.229', 27017)
+# client = MongoClient('mongodb://fellong:fellon@54.180.114.229', 27017)
+client = MongoClient('localhost', 27017)
 db = client.dbsparta
+
 
 # HTML을 주는 부분
 @app.route('/')
 def home():
-    return 'This is Home!'
-
-
-@app.route('/mypage')
-def mypage():
-    return render_template('index.html')
+    return render_template('fellonparty.html')
 
 
 # API 역할을 하는 부분
-@app.route('/post', methods=['POST'])
-def post():
-    url_receive = request.form['url_give']  # 클라이언트로부터 url을 받는 부분
-    comment_receive = request.form['comment_give']  # 클라이언트로부터 comment를 받는 부분
+@app.route('/apply', methods=['POST'])
+def apply():
+    name = request.form['name']
+    gender = request.form['gender']
+    count = request.form['count']
+    address = request.form['address']
+    phone = request.form['phone']
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
+    user = db.fellonparty.find_one({'name': name, 'phone': phone})
+    if user is not None:
+        return jsonify({
+            'success': 'false',
+            'message': 'This user is already applied'
+        })
 
-    soup = BeautifulSoup(data.text, 'html.parser')
+    data = {'name': name, 'gender': gender, 'count': count,
+               'address': address, 'phone': phone}
 
-    og_image = soup.select_one('meta[property="og:image"]')
-    og_title = soup.select_one('meta[property="og:title"]')
-    og_description = soup.select_one('meta[property="og:description"]')
+    db.fellonparty.insert_one(data)
 
-    url_image = og_image['content']
-    url_title = og_title['content']
-    url_description = og_description['content']
-
-    article = {'url': url_receive, 'comment': comment_receive,
-               'image': url_image, 'title': url_title, 'desc': url_description}
-
-    db.articles.insert_one(article)
-
-    return jsonify({'result': 'success'})
-
-
-@app.route('/fellongs', methods=['GET'])
-def view():
-    posts = db.articles.find({}, {'_id': 0})
-    return jsonify({'result': 'success', 'articles': list(posts)})
+    return jsonify({'success': 'true'})
 
 
 if __name__ == '__main__':
